5b78192b8eaac60ecbdeb84ba2671d567eb36248
Jeremy Tomasi <lugurbaldev@outlook.com>
2020-11-10 18:07:19 +0000

    ajout dossier cmake-build-debug

---
+++ debug/CMakeFiles/TP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP.dir/main.c.o"
+  "TP"
+  "TP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
