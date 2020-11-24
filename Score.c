ae527b90a91a5e53840a8488ba567fe95578a1c1
LucasLemort59 <73900699+LucasLemort59@users.noreply.github.com>
2020-11-24 16:21:06 +0100

    Ajout score.c

---
+++ Score.c
@@ -0,0 +1,26 @@
+
+// Score de la partie
+int score = 0;
+
+//Collisions si la balle tape la brique en bas
+if (centerPointX > brick.x && rightPointX < brick.x + brickWidth && brick.visible && posYBall <= brick.y + brickHeight) {
+    dy = -dy - speedBall;
+    brick.visible = false;
+    score++;
+}
+
+//Collisions côté droit de la brique
+if (posXBall < brick.x + brickWidth && centerMiddleY < brick.y + brickHeight && centerMiddleY > brick.y && brick.visible && rightPointX > brick.x + brickWidth) {
+dx = -dx - speedBall;
+brick.
+visible = false;
+score++;
+}
+
+//Collision côté gauche
+if (rightPointX > brick.x && centerMiddleY < brick.y + brickHeight && centerMiddleY > brick.y && brick.visible && rightPointX < brick.x + brickWidth){
+    dx = -dx - speedBall;
+    brick.visible = false;
+    score++;
+}
+
