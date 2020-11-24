8f724399c62237f17771033b4d7bf4cb1a8f4026
Samoul59 <73900715+Samoul59@users.noreply.github.com>
2020-11-24 17:13:37 +0100

    ajout struct.c

--- src/struct.c
+++ src/struct.c
@@ -3,3 +3,20 @@
 //
 
 #include "../include/struct.h"
+
+// Crée un objet pour chaque brique
+int nbLines=5;
+int nbBricks=12;
+for (int l = 0; l < nbLines; l++) {
+    int bricks[][];
+for (int b = 0; b < nbBricks; b++) {
+    int bricks[l][b] = {x: 0, y: 0, visible: true, couleur: null};
+}
+}
+
+// Permet de dessiner les briques
+typedef struct{
+    int x=2;
+    int y=20;
+    int couleur;
+} brique;
