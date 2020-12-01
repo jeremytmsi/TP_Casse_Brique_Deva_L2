0e09e01af5a7f863940d121c26d8bc4fb6c7894c
LucasLemort59 <73900699+LucasLemort59@users.noreply.github.com>
2020-12-01 18:35:11 +0100

    Score 2

---
+++ src/Sauvegarde.cpp
@@ -0,0 +1,4 @@
+//
+// Created by lucas on 01/12/2020.
+//
+
--- src/Score 2P.c
+++ src/Score 2P.c
@@ -1,11 +1,11 @@
 /*
 //int score2j(int centerPointX, brick, rightPointX, brickWidth, posYBall, brickHeight, dy, speedBall, dx, posXBall, centerMiddleY, nextBrick, undefined) {
 // Score de la partie
-char nom1[21] = {0};
+char nom1[19] = {0};
 printf("Nom du joueur1 ?");
 scanf("%s", nom1);
 int score1 = 0;
-char nom2[21] = {0};
+char nom2[19] = {0};
 printf("Nom du joueur2 ?");
 scanf("%s", nom2);
 int score2 = 0;
@@ -51,13 +51,46 @@ score2++;
 
 if (endgame = 1) {
     if (score1 < score2) {
-        printf("VAINQUEUR : nom2");
+        printf("VAINQUEUR : nom2\");
         else
-        printf("VAINQUEUR : nom1");
+        printf("VAINQUEUR : nom1\");
+        if (score1 = score2){
+        printf("Egalite !\"),
+        }
     }
     printf("SCORE %s: %d", nom1, score1);
     printf("SCORE %s: %d", nom2, score2);
 }
+
+//Tableau des top scores
+char TopNom[99][19] = {0};
+ for(int i = 0; i < 99;i++){
+    TopNom[i][0] = ' ';
+}
+int TopScore[99][0] = {0};
+ for(int i = 0; i < 99;i++){
+    TopScore[i][0] = 0;
+}
+
+ for (int i=0; i<=100; i++){
+    if (score1>TopScore[i][0]){
+        TopScore[i][0] = score1;
+        TopNom[i]=nom1;
+        }
+    if (score2>TopScore[i][0]){
+        TopScore[i][0] = score2;
+        TopNom[i]=nom2;
+        }
+    }
+
+for (int i=0; i<=99; i++){
+ if (TopScore[i][0]>0){
+ printf ("%c %d\", TopNom[i][0], TopScore[i][0]);
+ }
+}
+
+
+
 
 
 
