dab7ad4fe7118514b646517556a8dcab4a2a3293
Jeremy Tomasi <lugurbaldev@outlook.com>
2020-12-08 17:35:47 +0100

    Dessin raquette

--- include/dessin.h
+++ include/dessin.h
@@ -3,3 +3,7 @@
 
 void dessin(Brique **tabBriques, int nbLignes, int nbColonnes, SDL_Renderer *renderer);
 void creer_briques(Brique **tabBriques,int nbLignes, int nbColonnes);
+void dessiner_raquette(SDL_Renderer *renderer, Raquette raquette);
+Raquette *creer_raquette();
+void dessiner_raquette(SDL_Renderer *renderer, Raquette raquette);
+void capturer_event_keyboard(SDL_Event e, Raquette *raq);
--- src/main.c
+++ src/main.c
@@ -10,22 +10,29 @@ int main(int argc, char *argv[]) {
     SDL_Renderer *renderer = NULL;
     SDL_Event e;
 
+    int longueur = 640, largeur = 480;
     int nbLignes = 5, nbColonnes = 12;
 
-    window = creerFenetre("Casse-brique",640,480,SDL_WINDOW_SHOWN);
+    window = creerFenetre("Casse-brique",longueur,largeur,SDL_WINDOW_SHOWN);
     renderer = creerRendu(window);
 
-    afficherRendu(renderer);
-
-    char* mode = argv[1];
+    //afficherRendu(renderer);
 
     Brique **tabBriques = alloc_tab_briques(nbLignes,nbColonnes);
-    
-    creer_briques(tabBriques,nbLignes,nbColonnes);
 
+    Raquette *raquette = creer_raquette();
+
+    creer_briques(tabBriques,nbLignes,nbColonnes);
     while(continuer){
         dessin(tabBriques,nbLignes,nbColonnes,renderer);
-        SDL_WaitEvent(&e);
+        SDL_PollEvent(&e);
+        capturer_event_keyboard(e,raquette);
+        dessiner_raquette(renderer,*raquette);
+        SDL_RenderClear(renderer);
+        afficherRendu(renderer);
+
+
+        // Détecte l'appui clavier sur le bouton de fermeture
         switch(e.type) {
             case SDL_QUIT:
                 continuer = 0;
